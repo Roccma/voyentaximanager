d37620d3f2288ecf5e38fd708552ebfd33e0dec0
Unknown <mauroroccaf@gmail.com>
2018-06-08 05:12:49 -0300

    Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+io.on('connection', (socket) => {
+	socket.on('sessionid', (apiKey, apiSecret) => {
+		var OpenTok = require('opentok'),
+		opentok = new OpenTok(apiKey, apiSecret);
+		var sessionId;
+		opentok.createSession({mediaMode:"routed"}, function(error, session) {
+		  if (error) {
+		    io.emit('sessionid', "Error al crear session ID");
+		  } else {
+		    sessionId = session.sessionId;
+		    io.emit('sessionid', sessionId);
+		  }
+		});
+	});
+});
+
+http.listen(port, () => {
+	console.log("Servidor escuchando en el puerto " + port + "!");
+});
